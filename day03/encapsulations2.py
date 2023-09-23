74cda73b019cdefa3f7c35a3af14bdd2aa16f871
Hilal2013 <hilalozkul@gmail.com>
2023-09-23 10:13:44 -0400

    encapsulations2

--- day03/encapsulations2.py
+++ day03/encapsulations2.py
@@ -1,4 +1,11 @@
 class Person:
+
+    def __init__(self, name: str = 'Jimmy', age: int = 2):
+        self.__name = None
+        self.__age = None
+        self.person_name = name
+        self.person_age = age
+
     @property
     def person_name(self):
         if self.__name is None or self.__name == '' or type(self.__name) != str:
